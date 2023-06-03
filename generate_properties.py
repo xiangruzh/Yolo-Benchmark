0128a7d5d5dbb37ca42ccba86655c5d7cb45cd9d
xiangruzh <xiangruzh@126.com>
2023-06-03 17:08:14 +0800

    Update generate_properties.py

--- generate_properties.py
+++ generate_properties.py
@@ -35,6 +35,7 @@ if __name__ == '__main__':
         for selected_ind in selected:
             im_name = sample_list[selected_ind].split('.')[0]
             im = torch.load(os.path.join(input_path, sample_list[selected_ind]))
+            print(os.path.join(output_path, sample_list[selected_ind]))
             raw_output = torch.load(os.path.join(output_path, sample_list[selected_ind]))
             print(raw_output.shape)
             print('=================')
