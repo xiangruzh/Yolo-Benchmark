9d3950d1951a99e8454117e78c3dde8c5f9b6f72
xiangruzh <xiangruzh@126.com>
2023-06-07 00:05:17 +0800

    Update gen_vnnlib.py

--- gen_vnnlib.py
+++ gen_vnnlib.py
@@ -90,10 +90,9 @@ def gen_vnnlib(im, pred, im_name):
     perturb = torch.ones_like(im) * epsilon
     print(perturb)
     x_nat = inverse_normalize(im.detach().clone())
-    im_ub = normalize(torch.min(x_nat + perturb, torch.ones_like(im)))
-    im_lb = normalize(torch.max(x_nat - perturb, torch.zeros_like(im)))
+    im_ub = normalize(torch.max(torch.min(x_nat + perturb, torch.ones_like(im)), torch.zeros_like(im)))
+    im_lb = normalize(torch.max(torch.min(x_nat - perturb, torch.ones_like(im)), torch.zeros_like(im)))
     # print(x_net + perturb)
-    assert torch.all(torch.min(x_nat + perturb, torch.ones_like(im)) >= torch.max(x_nat - perturb, torch.zeros_like(im)))
     imf_ub = torch.flatten(im_ub)
     imf_lb = torch.flatten(im_lb)
     assert torch.all(imf_ub >= imf_lb)
